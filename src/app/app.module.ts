f655ea92da2c45e07ad53ed6e5060b1abdeb6462
ddleblanc <lxblvnc@gmail.com>
2018-01-15 16:14:28 +0100

    Dashboard added / Translation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,17 @@ import { InverterDetailsComponent } from './components/inverters/inverter-detail
 import { InverterService } from './services/inverter.service';
 import { MasterDataService } from './services/masterData.service';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,14 +54,25 @@ import { MasterDataService } from './services/masterData.service';
     EnergyEditComponent,
     InverterListComponent,
     InverterItemComponent,
-    InverterDetailsComponent
+    InverterDetailsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     EnergyService,
--- src/app/models/energyGain.model.ts
+++ src/app/models/energyGain.model.ts
@@ -1,13 +1,16 @@
 export class EnergyGain {
-    
+
     public _id
     public name: string;
     public energy: number;
     public time : string;
-      
+
     constructor(name: string, energy: number,time :string) {
         this.name = name;
         this.energy = energy;
         this.time = time;
+
+        
+
     }
-}  
+}
